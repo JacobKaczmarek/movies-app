1ea519e73fbd112bf43e6b8f72a77977ef503999
Jacob <kuba300698@gmail.com>
2019-11-09 02:52:08 +0100

    Add simple movie search and list of results

---
+++ src/components/movieCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="movieCardWrapper">
+    <img :src="`http://image.tmdb.org/t/p/w154//${movie.poster_path}`" alt />
+    <p>
+      <b>{{ movie.title }}</b>
+    </p>
+    <p>Popularity: {{ movie.popularity }}</p>
+    <p>{{ movie.vote_count }} Votes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: Object
+  },
+  data() {
+    return {
+      posterUrl: ''
+    };
+  },
+  methods: {
+    getPoster() {}
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.movieCardWrapper {
+  color: white;
+  width: 250px;
+  padding: 30px;
+  margin: 30px auto;
+  background: #333333;
+  border-radius: 3px;
+
+  img {
+    margin: 15px;
+  }
+}
+</style>
---
+++ src/components/moviesList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="moviesListWrapper">
+    <h2>Search for a movie</h2>
+    <b-form-input placeholder="Movie title" v-model="title"></b-form-input>
+    <b-button @click="searchMovie()">Search</b-button>
+    <div class="list-container">
+      <movieCard v-for="movie in movies" :key="movie.id" :movie="movie"></movieCard>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { apiToken } from '../../credentials.json';
+import movieCard from './movieCard.vue';
+
+const apiUrl = 'https://api.themoviedb.org/3';
+
+export default {
+  components: {
+    movieCard
+  },
+  data() {
+    return {
+      movies: [],
+      title: ''
+    };
+  },
+  methods: {
+    searchMovie() {
+      // Get movies list
+      axios
+        .get(`${apiUrl}/search/movie?api_key=${apiToken}&query=${this.title}`)
+        .then(async data => {
+          this.movies = data.data.results;
+          console.log(this.movies);
+        })
+        .catch(err => console.error(err));
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.moviesListWrapper {
+  width: 70%;
+  margin: 23vh auto;
+
+  input {
+    margin: 30px 0 30px;
+  }
+  .list-container {
+    display: grid;
+    grid-template-columns: auto auto auto;
+  }
+}
+</style>
